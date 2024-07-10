e5b7732764951319070576cb750d5c1962fbafe5
Safeer Aslam <safeeraslamc@gmail.com>
2024-07-10 13:02:04 +0530

    Added OTP login functions in AuthController, Added new api guard

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\API\BaseController as BaseController;
 use App\Models\User;
+use App\Notifications\SendOtp;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class AuthController extends BaseController
 {
@@ -20,22 +22,17 @@ class AuthController extends BaseController
     {
         $validator = Validator::make($request->all(), [
             'name' => 'required',
-            'email' => 'required|email',
-            'password' => 'required',
-            'c_password' => 'required|same:password',
+            'email' => 'required|email|unique'
         ]);
-   
+        
         if($validator->fails()){
             return $this->sendError('Validation Error.', $validator->errors());       
         }
-   
+
         $input = $request->all();
-        $input['password'] = bcrypt($input['password']);
-        $user = User::create($input);
-        $success['token'] =  $user->createToken('MyApp')->plainTextToken;
-        $success['name'] =  $user->name;
-   
-        return $this->sendResponse($success, 'User register successfully.');
+        //$user = User::create($input);
+        
+        //$this->sendOtp($request->email);
     }
    
     /**
@@ -55,5 +52,74 @@ class AuthController extends BaseController
         else{ 
             return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
         } 
+    }
+
+    /**
+     * Sending Login OTP to email
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function sendOtp(Request $request)
+    {
+        $data = $request->validate(
+            [
+                'email' => ['bail', 'required', 'email', Rule::exists(User::class, 'email')],
+            ]
+        );
+
+        $user = User::where('email', $data['email'])->firstOrFail();
+
+        $otp = $user->otp()->firstOrCreate([], [
+            'token' => rand(1000, 9999),
+        ]);
+
+        $otp->load('authable');
+
+        $user->notify(new SendOtp($otp));
+
+        $success['otp_sent'] = true;
+
+        return $this->sendResponse($success, 'OTP send successfully.');
+    }
+
+    /**
+     * Verifying Login OTP
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function verifyOtp(Request $request)
+    {
+        $data = $request->validate(
+            [
+                'email' => ['bail', 'required', 'email', Rule::exists(User::class, 'email')],
+                'otp' => 'bail|required|integer'
+            ]
+        );
+
+        $user = User::where('email', $data['email'])->with('otp')->firstOrFail();
+
+        if($user->otp == null){
+            return $this->sendError('Unauthorized.', ['error'=>'Otp not available. Request new one and try again.']);
+        }
+
+        if($user->otp->token === (int) $data['otp']){
+
+            
+            // Verifying email if not done already(Using in registration time).
+            if($user->email_verified_at == null){
+                $user->update([
+                    'email_verified_at' => now()
+                ]);
+            }
+            
+
+            $user->otp()->delete();
+            $success['token'] = $user->createToken(env('APP_NAME'))->plainTextToken;
+            $success['name'] = $user->name;
+
+            return $this->sendResponse($success, 'User logged in successfully.');
+        }else{
+            return $this->sendError('Unauthorized.', ['error'=>'Unauthorized']);
+        }
     }
 }
