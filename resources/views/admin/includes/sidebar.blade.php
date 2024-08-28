642aadcb9949d726e2386aeded0721691d42fe44
Safeer Aslam <safeeraslamc@gmail.com>
2024-08-28 22:17:42 +0530

    Hide roles and users

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -4,11 +4,13 @@
         @if(Module::has('Members'))
             @include('members::admin.sidebarmenu')
         @endif
+        <!-- 
         @canany(['role.create', 'role.view','role.edit','role.delete','role.approve'])
             <a href="/admin/roles" class="nav-link">Roles</a>
         @endcan
         @canany(['user.create', 'user.view','user.edit','user.delete','user.approve'])
             <a href="/admin/users" class="nav-link">Users</a>
         @endcan
+        -->
     </nav>
 </div>
