21c82492e13bb9695fbf238c0404cdd57fbc60db
Safeer Aslam <safeeraslamc@gmail.com>
2024-11-06 12:37:21 +0530

    Updated events api

--- Modules/Members/resources/views/includes/idcard.blade.php
+++ Modules/Members/resources/views/includes/idcard.blade.php
@@ -3,6 +3,14 @@
         <img src="{{ url('storage/images/'. $member->user->avatar) }}" alt="{{ $member->user->name }}" title="{{ $member->user->name }}"/>
     </div>
     <div class="name">{{ $member->name }}</div>
+    <div class="title-info">
+        <span class="item">
+            <i class="fa-solid fa-phone"></i> +{{$member->user->calling_code}}{{$member->user->phone}}
+        </span>
+        <span class="item">
+            <i class="fa-solid fa-droplet"></i> {{$member->blood_group}}
+        </span>
+    </div>
     <div class="id-wrapper">
         <div class="item id">
             <span class="label">Mem. ID</span>
@@ -30,7 +38,7 @@
     <div class="footer">
         <div class="item">
             <div class="label">MEMBER {{ $member->type == 'primary' ? '('.$member->type.')': ''}}</div>
-            <div class="expiry">Expired on {{ $member->membership->expiry }}</div>
+            <div class="expiry">Expired on {{ date('d M, Y', strtotime($member->membership->expiry_date)) }}</div>
         </div>
         <div class="item">
             <div class="status {{ $member->membership->status }}">{{ $member->membership->status }}</div>
