af4cc5d3717d26382f02e1d447ecea2b846911d3
Safeer Aslam <safeeraslamc@gmail.com>
2025-02-03 03:55:37 +0530

    Added expiry date in member export

--- Modules/Members/resources/views/admin/includes/profile/actions.blade.php
+++ Modules/Members/resources/views/admin/includes/profile/actions.blade.php
@@ -9,7 +9,6 @@
         <li>
             <a href="#" class="btn btn-xs btn-outline-default dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="icon" data-feather="file"></i> Export</a>
             <ul class="dropdown-menu dropdown-menu-end">
-                <li><a href="/admin/members/member/pdf/{{ $member->user->id }}" class="dropdown-item">Export to PDF</a></li>
                 <li><a href="/admin/members/member/excel/{{ $member->user->id }}" class="dropdown-item">Export to Excel</a></li>
             </ul>
         </li>
