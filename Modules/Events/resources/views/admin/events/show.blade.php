ca76addfdb93c03028dc87a42958210e6d857dbf
Safeer Aslam <safeeraslamc@gmail.com>
2025-03-29 10:42:19 +0530

    updated event view

--- Modules/Events/resources/views/admin/events/show.blade.php
+++ Modules/Events/resources/views/admin/events/show.blade.php
@@ -16,6 +16,10 @@
             <div class="event-header">
                 <div class="event-title">
                     <h1 class="title">{{$event->title}}</h1>
+                    <p>{{$event->description}}</p>
+                    <div>Date: {{date('M d, Y', strtotime($event->start_date))}} @if($event->end_date ) -  {{date('M d, Y', strtotime($event->end_date))}}</->@endif</div>
+                    @if($event->start_time )<div>Time: {{date('h:i a', strtotime($event->start_time))}} @if($event->end_time ) - {{date('h:i a', strtotime($event->end_time))}}@endif </div>@endif
+                    @if($event->location)<div>Venue: {{$event->location}}</div>@endif
                 </div>
                 <div>
                     <a href="/admin/events/{{$event->id}}/volunteers" class="btn btn-xs btn-primary">View Volunteers</a>
@@ -24,25 +28,14 @@
             </div>
             <ul class="nav nav-underline" id="eventTab" role="tablist">
                 <li class="nav-item" role="presentation">
-                    <button class="nav-link active" id="tab_overview" data-bs-toggle="tab" data-bs-target="#tab_overview-pane" type="button" role="tab" aria-controls="tab_overview-pane" aria-selected="true">Overview</button>
+                    <a href="#tab-participants" class="nav-link active" id="tab_participants" data-bs-toggle="tab" data-bs-target="#tab_participants-pane" role="tab" aria-controls="tab_participants-pane" aria-selected="false">Participants</a>
                 </li>
                 <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="tab_participants" data-bs-toggle="tab" data-bs-target="#tab_participants-pane" type="button" role="tab" aria-controls="tab_participants-pane" aria-selected="false">Participants</button>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="tab_volunteer" data-bs-toggle="tab" data-bs-target="#tab_volunteer-pane" type="button" role="tab" aria-controls="tab_volunteer-pane" aria-selected="false">Volunteers</button>
+                    <a href="#tab-volunteers" class="nav-link" id="tab_volunteer" data-bs-toggle="tab" data-bs-target="#tab_volunteer-pane" role="tab" aria-controls="tab_volunteer-pane" aria-selected="false">Volunteers</a>
                 </li>
             </ul>
             <div class="tab-content" id="myTabContent">
-                <div class="tab-pane fade show active" id="tab_overview-pane" role="tabpanel" aria-labelledby="tab_overview" tabindex="0">
-                    <p>{{$event->description}}</p>
-                    <div>Start Date: {{date('M d,Y', strtotime($event->start_date))}}</div>
-                    @if($event->end_date )<div>Start Date: {{date('M d,Y', strtotime($event->end_date))}}</div>@endif
-                    @if($event->start_time )<div>Start Time: {{date('h:i a', strtotime($event->start_time))}}</div>@endif
-                    @if($event->end_time )<div>Start Time: {{date('h:i a', strtotime($event->end_time))}}</div>@endif
-                    @if($event->location)<div>Venue: {{$event->location}}</div>@endif
-                </div>
-                <div class="tab-pane fade" id="tab_participants-pane" role="tabpanel" aria-labelledby="tab_participants" tabindex="0">
+                <div class="tab-pane fade show active" id="tab_participants-pane" role="tabpanel" aria-labelledby="tab_participants" tabindex="0">
                     <table class="table event-list">
                         <thead>
                             <tr>
@@ -63,6 +56,7 @@
                             @endforeach
                         </tbody>
                     </table>
+                    <div class="pagination-container">{{ $participants->appends(request()->query())->links() }}</div>
                 </div>
                 <div class="tab-pane fade" id="tab_volunteer-pane" role="tabpanel" aria-labelledby="tab_volunteer" tabindex="0">
                     <table class="table event-list">
