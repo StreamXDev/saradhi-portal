c5d574a6bda84542615f24603addd8534f060558
Safeer Aslam <safeeraslamc@gmail.com>
2025-04-08 18:04:14 +0530

    Updated notification module

--- Modules/PushNotification/resources/views/backend/sidebarmenu.blade.php
+++ Modules/PushNotification/resources/views/backend/sidebarmenu.blade.php
@@ -1,1 +1,7 @@
+@canany([
+    'notification.create',
+    'notification.send',
+    'notification.history'
+])
 <li><a href="/admin/push-notification" class="nav-item @if(isset($menuParent) && $menuParent == 'notification') active @endif" ><i class="icon" data-feather="bell"></i>Push Notification</a></li>
+@endcanany
