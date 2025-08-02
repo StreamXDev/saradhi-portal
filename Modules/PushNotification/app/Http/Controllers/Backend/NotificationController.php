a239655decd046afed219e243025f613580c1162
Safeer Aslam <safeeraslamc@gmail.com>
2025-08-02 09:05:20 +0530

    Fixing test notification2

--- Modules/PushNotification/app/Http/Controllers/Backend/NotificationController.php
+++ Modules/PushNotification/app/Http/Controllers/Backend/NotificationController.php
@@ -150,7 +150,7 @@ class NotificationController extends Controller
                     ],
                     "data" => [
                         "screen" => "notification",
-                        "id" => $notificationId,
+                        "id" => "22",
                     ],
                 ]
             ];
