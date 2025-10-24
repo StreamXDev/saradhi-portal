769e5212d58eca6f43bcefbe92bd2b51004cd301
Safeer Aslam <safeeraslamc@gmail.com>
2025-10-24 21:35:18 +0530

    Updated exports

--- Modules/Members/app/Exports/MembersListExport.php
+++ Modules/Members/app/Exports/MembersListExport.php
@@ -45,7 +45,7 @@ class MembersListExport implements FromCollection, WithHeadings, WithMapping, Sh
             '+'.$member->user->calling_code.$member->user->phone,
             $member->details->whatsapp ? '+'.$member->details->whatsapp_code.$member->details->whatsapp : '',
             $member->details->emergency_phone ? '+'.$member->details->emergency_phone_code.$member->details->emergency_phone : '',
-            $member->membership->type,
+            $member->membership->family_in == 'kuwait' ? 'Family' : 'Single',
             date('d-m-Y', strtotime($member->membership->start_date)),
             date('d-m-Y', strtotime($member->membership->expiry_date)),
             $member->details->civil_id.' ',
