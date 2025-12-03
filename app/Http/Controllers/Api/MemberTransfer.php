2c1847f5bf767645b4b3fbdf3c6e5c1d747654a9
Safeer Aslam <safeeraslamc@gmail.com>
2025-12-03 07:50:21 +0530

    Adding transfer

--- app/Http/Controllers/Api/MemberTransfer.php
+++ app/Http/Controllers/Api/MemberTransfer.php
@@ -10,6 +10,15 @@ use Illuminate\Support\Facades\Auth;
 class MemberTransfer extends BaseController
 {
 
+    public function getUser($id)
+    {
+        $user = User::where('id', $id)->first();
+        $data = [
+            'users' => $user
+        ];
+        return $this->sendResponse($data, 'User.');
+    }
+
     public function getUsersAfterId($id)
     {
         $users = User::where('id', '>', $id)->take(5)->get();
