dbded599e707cc7aa4ad059efa413ddda7fafce5
Safeer Aslam <safeeraslamc@gmail.com>
2025-12-05 23:17:53 +0530

    Updated avatar resize

--- app/Http/Controllers/Api/MemberTransfer.php
+++ app/Http/Controllers/Api/MemberTransfer.php
@@ -46,12 +46,23 @@ class MemberTransfer extends BaseController
         $localAddress = MemberLocalAddress::where('user_id', $user->id)->first();
         $permanentAddress = MemberPermanentAddress::where('user_id', $user->id)->first();
         $membership_request = MembershipRequest::where('user_id', $user->id)->get();
-        /**
-         * if member->id relations
-         * if related member id,
-         * get user from related member id
-         */
         $relations = MemberRelation::where('member_id', $member->id)->get();
+
+        // Reducing avatar image size
+        if($user->avatar){
+            $source_image_path = storage_path('app/public/images/'.$user->avatar);
+            $destination_image_path = storage_path('app/public/images/'.$user->avatar);
+            list($width, $height) = getimagesize($source_image_path);
+            $new_width = 300;
+            $new_height = 300;
+            $new_image = imagecreatetruecolor($new_width, $new_height);
+            $source_image = imagecreatefromjpeg($source_image_path);
+            imagecopyresampled($new_image, $source_image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+            imagejpeg($new_image, $destination_image_path, 90);
+            $source_image = null;
+            $new_image = null;
+        }
+
         foreach($relations as $relation){
             if($relation->related_member_id){
                 $relation->type = 'user';
