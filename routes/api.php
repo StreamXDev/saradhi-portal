f7dc38186b1a2570491e82048e07a03f3cd6131b
Safeer Aslam <safeeraslamc@gmail.com>
2026-05-26 15:38:42 +0530

    Updated member transfer

--- routes/api.php
+++ routes/api.php
@@ -45,6 +45,7 @@ Route::middleware(['auth:sanctum','verified_email'])->group(function () {
 Route::group(['middleware' => ['auth:sanctum', 'verified_email', 'is_admin']], function() {
     Route::controller(MemberTransfer::class)->prefix('transfer')->group(function(){
         Route::get('user/{id}', 'getUser');
+        Route::get('allusers', 'getUsers');
         Route::get('users/{id}/{?count}', 'getUsersAfterId');
         Route::get('member/{id}', 'getMember');
     });
